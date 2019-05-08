4c5a7093315287630ed4360f9d54a442717e8ea2
Lucjan Kępiński <lucjan.kepinski@gmail.com>
2019-05-08 21:27:13 +0200

    javascript 8

--- test.js
+++ test.js
@@ -1,20 +1,40 @@
-var myVariable_1;
+var myVar = 5
 // undefined
-myVariable_1 = 10;
-// 10
-var myVar2 = "hello";
-// undefined
-myVar2
-// "hello"
-myVar2 = "hello again";
-// "hello again"
-myVar2
-// "hello again"
-myVar2 = 5;
+myVar
 // 5
-var customerAge = 32;
-// undefined
-customerAge
-// 32
-var myVar3 = 40;
-// undefined
+5 + 5
+// 10
+myVar + 10
+// 15
+myVar
+// 5
+myVar = myVar + 10
+// 15
+myVar
+// 15
+10 -5
+// 5
+myVar - 3
+// 12
+myVar = myVar - 5
+// 10
+myVar
+// 10
+10 * 10
+// 100
+20 / 10
+// 2
+myVar = myVar / 2
+// 5
+myVar
+// 5
+5 + "hello"
+// "5hello"
+100 + "Hello"
+// "100Hello"
+"hello" + "david"
+// "hellodavid"
+"hello" + " david"
+// "hello david"
+5 * "hello"
+// NaN
