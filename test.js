a74e34234415c50c0f5274e91bfdfdbce1039dda
Lucjan Kępiński <lucjan.kepinski@gmail.com>
2019-05-08 22:07:25 +0200

    javascript 12

--- test.js
+++ test.js
@@ -1,32 +1,11 @@
-var iLikeMeat = true
-// undefined
-iLikeMeat
-// true
-iLikeMeat = "true"
-// "true"
-iLikeMeat = false
-// false
-7 > 5
-// true
-7 < 5
-// false
-7 = 7
-// VM2671:1 Uncaught ReferenceError: Invalid left-hand side in assignment
-7 == 7
-// true
-7 == 5
-// false
-0
-// 0
-Boolean(7 > 5)
-// true
-Boolean(6)
-// true
-Boolean(-5)
-// true
-Boolean(0)
-// false
-Boolean("hello")
-// true
-Boolean("")
-// false
+var youLikeMeat = true;
+var myNum = 10;
+
+if (youLikeMeat){
+
+    document.write("you like meat");
+
+} else{
+
+    document.write("you hate meat");
+}
