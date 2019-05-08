0d772926866be40d55bf56403228c9a38d6aa020
Lucjan Kępiński <lucjan.kepinski@gmail.com>
2019-05-08 21:19:57 +0200

    javascript 7

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
-// the first alert
-alert("Hello, World!");
-
-// the second alert
-alert("hi again");
-
-
-//make the image invisible
-//move the image from top to bottom
-//make the image visible
+var myVariable_1;
+// undefined
+myVariable_1 = 10;
+// 10
+var myVar2 = "hello";
+// undefined
+myVar2
+// "hello"
+myVar2 = "hello again";
+// "hello again"
+myVar2
+// "hello again"
+myVar2 = 5;
+// 5
+var customerAge = 32;
+// undefined
+customerAge
+// 32
+var myVar3 = 40;
+// undefined
