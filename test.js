bcbe19c72e8af16d0775b7d2d9d48f676c35028c
Lucjan Kępiński <lucjan.kepinski@gmail.com>
2019-05-08 21:09:35 +0200

    javascript 6

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
+// the first alert
 alert("Hello, World!");
+
+// the second alert
+alert("hi again");
+
+
+//make the image invisible
+//move the image from top to bottom
+//make the image visible
